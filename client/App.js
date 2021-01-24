9954b509b4364ead1c424ba81e9c630341942f8f
jairoPonti <jairmponti@gmail.com>
2021-01-24 20:30:34 -0300

    Revisar px y falta autenticar desde el front

--- client/App.js
+++ client/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import LandingPage from './screens/LandingPage';
+import CreateAccount from './screens/CreateAccount';
 
 export default function App() {
 
@@ -11,6 +12,7 @@ export default function App() {
     <NavigationContainer>
       <Drawer.Navigator initialRouteName="LandingPage">
         <Drawer.Screen name="LandingPage" component={LandingPage} />
+        <Drawer.Screen name="CreateAccount" component={CreateAccount} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
