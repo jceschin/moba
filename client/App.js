357e7168c0137037c4c8f188fcca078bb57897bf
Juan Ceschin <juanceschin@gmail.com>
2021-01-24 14:11:32 -0300

    Reordered folders

--- client/App.js
+++ client/App.js
@@ -1,175 +1,19 @@
 import React from 'react';
-import { StyleSheet, View, Text, ScrollView } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
-import { AntDesign, MaterialIcons, Feather } from '@expo/vector-icons';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import LandingPage from './screens/LandingPage';
+import Homepage from './screens/Homepage';
 
-const Homepage = () => {
+export default function App() {
+
+  const Drawer = createDrawerNavigator();
+
   return (
-    <LinearGradient
-    style={styles.container}
-    colors={['rgba(140, 165, 253, 1)', 'rgba(243, 129, 245, 0.77)']}
-    start={{ x: 0, y: 0 }}
-    end={{ x: 1, y: 1 }}
-    >
-      <ScrollView>
-        <Text style={styles.accountOwner}>Juan Ceschin</Text>
-        <Text style={styles.balanceTag}>Money</Text>
-        <Text style={styles.balance}>US$ 5,000</Text>
-        <View style={styles.options}>
-          <View>
-            <AntDesign name="plus" size={24} color="black" style={styles.optionIcon}/>
-            <Text style={styles.option}>Add Money</Text>
-          </View>
-          <View>
-            <MaterialIcons name="arrow-right-alt" size={24} color="black" style={styles.optionIcon}/>
-            <Text style={styles.option}>Transfer</Text>
-          </View>
-          <View>
-            <Feather name="settings" size={24} color="black" style={styles.optionIcon}/>
-            <Text style={styles.option}>Settings</Text>
-          </View>
-        </View>
-        <View style={styles.movsContainer}>
-          <Text style={styles.movsHeader}>Last Movements</Text>
-          <View style={styles.mov}>
-            <View style={styles.movDateContainer}>
-              <Text style={styles.movDate}>January 2021</Text>
-            </View>
-            <View style={styles.movDetails}>
-              <Text style={styles.movType}>Type of transfer</Text>
-              <Text style={styles.movAmount}>US$999.99</Text>
-            </View>
-          </View>
-          <View style={styles.mov}>
-            <View style={styles.movDateContainer}>
-              <Text style={styles.movDate}>January 2021</Text>
-            </View>
-            <View style={styles.movDetails}>
-              <Text style={styles.movType}>Type of transfer</Text>
-              <Text style={styles.movAmount}>US$999.99</Text>
-            </View>
-          </View>
-          <View style={styles.mov}>
-            <View style={styles.movDateContainer}>
-              <Text style={styles.movDate}>January 2021</Text>
-            </View>
-            <View style={styles.movDetails}>
-              <Text style={styles.movType}>Type of transfer</Text>
-              <Text style={styles.movAmount}>US$999.99</Text>
-            </View>
-          </View>
-        </View>
-      </ScrollView>
-    </LinearGradient>
-  )
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="LandingPage">
+        <Drawer.Screen name="LandingPage" component={LandingPage} />
+        <Drawer.Screen name="Homepage" component={Homepage} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
 }
-
-export default Homepage;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  accountOwner: {
-    fontSize: 36,
-    textAlign: 'center',
-    fontWeight: 'bold',
-    top: 31
-  },
-  balanceTag: {
-    fontSize: 20,
-    textAlign: 'center',
-    fontWeight: 'normal',
-    top: 70
-  },
-  balance: {
-    fontSize: 36,
-    textAlign: 'center',
-    fontWeight: 'bold',
-    top: 100
-  },
-  options: {
-    flexDirection: 'row',
-    top: 140,
-    justifyContent: 'space-between',
-    paddingLeft: 18,
-    paddingRight: 18
-  },
-  option: {
-    fontSize: 16
-  },
-  optionIcon: {
-    textAlign: 'center',
-    marginBottom: 5
-  },
-  movsContainer: {
-    top: 160,
-    backgroundColor: 'white',
-    height: '100%',
-    borderTopLeftRadius: 10,
-    borderTopRightRadius: 10,
-  },
-  movsHeader: {
-    marginTop: 36,
-    fontSize: 18,
-    textAlign: 'center',
-    fontWeight: 'normal'
-  },
-  mov: {
-    marginTop: 10,
-    marginBottom: 10,
-  },
-  movDate: {
-    fontSize: 18,
-    textAlign: 'left',
-    fontWeight: 'normal',
-    borderBottomWidth: 1,
-    borderBottomColor: 'grey',
-    borderStyle: 'solid',
-    marginBottom: 5
-  },
-  movDateContainer: {
-    borderBottomWidth: 1,
-    borderBottomColor: 'grey',
-    borderStyle: 'solid',
-    paddingLeft: 18,
-    paddingRight: 18
-  },
-  movDetails: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    paddingLeft: 18,
-    paddingRight: 18,
-    marginTop: 5
-  },
-  movType: {
-    fontSize: 14,
-    textAlign: 'left',
-    fontWeight: 'normal'
-  },
-  movAmount: {
-    fontSize: 14,
-    textAlign: 'right',
-    fontWeight: 'normal'
-  }
-})
-
-// import React from 'react';
-// import { createDrawerNavigator } from '@react-navigation/drawer';
-// import { NavigationContainer } from '@react-navigation/native';
-// import LandingPage from './screens/LandingPage';
-// import Homepage from './screens/Homepage';
-
-// export default function App() {
-
-//   const Drawer = createDrawerNavigator();
-
-//   return (
-//     <NavigationContainer>
-//       <Drawer.Navigator initialRouteName="LandingPage">
-//         <Drawer.Screen name="LandingPage" component={LandingPage} />
-//         <Drawer.Screen name="Homepage" component={Homepage} />
-//       </Drawer.Navigator>
-//     </NavigationContainer>
-//   );
-// }
--- client/screens/Homepage.jsx
+++ client/screens/Homepage.jsx
@@ -1,26 +1,66 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, ScrollView } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
+import { AntDesign, MaterialIcons, Feather } from '@expo/vector-icons';
 
 const Homepage = () => {
   return (
-      <View>
-        <Text>Juan Ceschin</Text>
-        <Text>Money</Text>
-        <Text>US$ 5,000</Text>
-        <View>
-          <Text>Add Money</Text>
-          <Text>Transfer</Text>
-          <Text>Settings</Text>
-        </View>
-        <View>
-          <Text>Last Movements</Text>
+    <LinearGradient
+    style={styles.container}
+    colors={['rgba(140, 165, 253, 1)', 'rgba(243, 129, 245, 0.77)']}
+    start={{ x: 0, y: 0 }}
+    end={{ x: 1, y: 1 }}
+    >
+      <ScrollView>
+        <Text style={styles.accountOwner}>Juan Ceschin</Text>
+        <Text style={styles.balanceTag}>Money</Text>
+        <Text style={styles.balance}>US$ 5,000</Text>
+        <View style={styles.options}>
           <View>
-            <Text>January 2021</Text>
-            <Text>US$999.99</Text>
+            <AntDesign name="plus" size={24} color="black" style={styles.optionIcon}/>
+            <Text style={styles.option}>Add Money</Text>
+          </View>
+          <View>
+            <MaterialIcons name="arrow-right-alt" size={24} color="black" style={styles.optionIcon}/>
+            <Text style={styles.option}>Transfer</Text>
+          </View>
+          <View>
+            <Feather name="settings" size={24} color="black" style={styles.optionIcon}/>
+            <Text style={styles.option}>Settings</Text>
           </View>
         </View>
-      </View>
+        <View style={styles.movsContainer}>
+          <Text style={styles.movsHeader}>Last Movements</Text>
+          <View style={styles.mov}>
+            <View style={styles.movDateContainer}>
+              <Text style={styles.movDate}>January 2021</Text>
+            </View>
+            <View style={styles.movDetails}>
+              <Text style={styles.movType}>Type of transfer</Text>
+              <Text style={styles.movAmount}>US$999.99</Text>
+            </View>
+          </View>
+          <View style={styles.mov}>
+            <View style={styles.movDateContainer}>
+              <Text style={styles.movDate}>January 2021</Text>
+            </View>
+            <View style={styles.movDetails}>
+              <Text style={styles.movType}>Type of transfer</Text>
+              <Text style={styles.movAmount}>US$999.99</Text>
+            </View>
+          </View>
+          <View style={styles.mov}>
+            <View style={styles.movDateContainer}>
+              <Text style={styles.movDate}>January 2021</Text>
+            </View>
+            <View style={styles.movDetails}>
+              <Text style={styles.movType}>Type of transfer</Text>
+              <Text style={styles.movAmount}>US$999.99</Text>
+            </View>
+          </View>
+        </View>
+      </ScrollView>
+    </LinearGradient>
   )
 }
 
@@ -29,81 +69,87 @@ export default Homepage;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 10,
-    justifyContent: 'center',
   },
+  accountOwner: {
+    fontSize: 36,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    top: 31
+  },
+  balanceTag: {
+    fontSize: 20,
+    textAlign: 'center',
+    fontWeight: 'normal',
+    top: 70
+  },
+  balance: {
+    fontSize: 36,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    top: 100
+  },
+  options: {
+    flexDirection: 'row',
+    top: 140,
+    justifyContent: 'space-between',
+    paddingLeft: 18,
+    paddingRight: 18
+  },
+  option: {
+    fontSize: 16
+  },
+  optionIcon: {
+    textAlign: 'center',
+    marginBottom: 5
+  },
+  movsContainer: {
+    top: 160,
+    backgroundColor: 'white',
+    height: '100%',
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+  },
+  movsHeader: {
+    marginTop: 36,
+    fontSize: 18,
+    textAlign: 'center',
+    fontWeight: 'normal'
+  },
+  mov: {
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  movDate: {
+    fontSize: 18,
+    textAlign: 'left',
+    fontWeight: 'normal',
+    borderBottomWidth: 1,
+    borderBottomColor: 'grey',
+    borderStyle: 'solid',
+    marginBottom: 5
+  },
+  movDateContainer: {
+    borderBottomWidth: 1,
+    borderBottomColor: 'grey',
+    borderStyle: 'solid',
+    paddingLeft: 18,
+    paddingRight: 18
+  },
+  movDetails: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingLeft: 18,
+    paddingRight: 18,
+    marginTop: 5
+  },
+  movType: {
+    fontSize: 14,
+    textAlign: 'left',
+    fontWeight: 'normal'
+  },
+  movAmount: {
+    fontSize: 14,
+    textAlign: 'right',
+    fontWeight: 'normal'
+  }
 })
-
-// const AccountOwner = styled.Text`
-//   font-size:36px;
-//   text-align:center;
-//   font-weight:bold;
-//   top:31px;
-// `;
-
-// const BalanceTag = styled.Text`
-//   font-size:20px;
-//   text-align:center;
-//   font-weight:normal;
-//   top:119px;
-// `;
-
-// const Balance = styled.Text`
-//   font-size:36px;
-//   text-align:center;
-//   font-weight:bold;
-//   top:171px;
-// `;
-
-// const Options = styled.View`
-//   display:flex;
-//   top: 240px;
-//   justify-content:space-between;
-// `;
-
-// const OptionName = styled.Text`
-//   font-size:16px;
-// `;
-
-// const MovsContainer = styled.View`
-//   top:342px;
-//   background-color:#FDF5E6;
-//   height:100%;
-//   border-top-left-radius:10px;
-//   border-top-right-radius:10px;
-// `;
-
-// const MovsHeader = styled.Text`
-//   margin-top:36px;
-//   font-size:18px;
-//   text-align:center;
-//   font-weight:normal;
-// `;
-
-// const Mov = styled.View`
-//   margin-top:60px;
-//   margin-bottom:60px;
-// `;
-
-// const MovDate = styled.Text`
-//   font-size:18px;
-//   text-align:left;
-//   font-weight:normal;
-// `;
-
-// const MovType = styled.Text`
-//   font-size:14px;
-//   text-align:left;
-//   font-weight:normal;
-// `;
-
-// const MovAmount = styled.Text`
-//   font-size:14px;
-//   text-align:right;
-//   font-weight:normal;
-// `;
-
-// const MovDetails = styled.View`
-//   display:flex;
-//   justify-content:space-between;
-// `;
