0e641dad32a9b691010d8f0e16b55a9bd6907ebc
Lucas G <g.brianlucas1993@gmail.com>
2021-01-25 13:19:59 -0300

    routes for accounts

---
+++ api/microservices/accounts.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const server = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const { Accounts } = require('../db');
+
+// middlewares
+server.use(morgan('dev'));
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true}));
+
+//Create account
+
+server.post("/accounts", (req,res,next) => {
+
+    Accounts.create(req.body)
+
+    .then((accounts) =>{
+
+        res.status(201).send(accounts);
+
+    })
+    .catch(err => { res.status(404).send(err) });
+
+});
+
+// get all accounts
+
+server.get("/accounts", (req, res, next) => {
+
+    Accounts.findAll()
+    .then((accounts) => {
+        res.send(accounts);
+    })
+    .catch(err => { res.status(404).send(err) });
+
+});
+
+// get specific account by cvu
+
+server.get("/accounts/:cvu", (req, res, next) => {
+
+    Accounts.findOne({
+        where:{
+            cvu: req.params.cvu
+        }
+    })
+    .then(account => res.send(account))
+    .catch(err => { res.status(404).send(err) });
+
+});
+
+// update accounts by cvu
+
+server.put("/accounts/:cvu", (req, res) => {
+    Accounts.update(req.body,
+        {
+            where:  {cvu: req.params.cvu }
+        }
+        )
+    .then(account => res.status(200).send(account))
+    .catch(err => { res.status(404).send(err) });
+});
+
+server.listen(8001, () => {
+    console.log("Server running on 8001");
+});
+
+module.exports = server;
