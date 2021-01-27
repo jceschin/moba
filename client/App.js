0d84d3d6765ab52e48f57a64e10036b924d4de34
Juan Ceschin <juanceschin@gmail.com>
2021-01-27 18:24:34 -0300

    All done

--- client/App.js
+++ client/App.js
@@ -7,7 +7,11 @@ import LandingPage from './screens/LandingPage';
 import RegisterPage from './screens/RegisterPage';
 import LastRegisterPage from './screens/LastRegisterPage';
 import CreateAccount from './screens/CreateAccount';
+import {Provider} from 'react-redux';
+import generateStore from './Store'
 import Homepage from './screens/Homepage';
+import HomeNavbar from './screens/HomeNavbar';
+import MyAccount from './screens/MyAccount';
 
 // REDUX
 import { Provider } from 'react-redux';
@@ -26,11 +30,14 @@ export default function App() {
       <NavigationContainer>
         <Drawer.Navigator initialRouteName='LandingPage'>
           <Drawer.Screen name='LandingPage' component={LandingPage} />
-        <Drawer.Screen name="CreateAccount" component={CreateAccount} />
+          <Drawer.Screen name="CreateAccount" component={CreateAccount} />
           <Drawer.Screen name='RegisterPage' component={RegisterPage} />
           <Drawer.Screen name='LastRegisterPage' component={LastRegisterPage} />
           <Drawer.Screen name="Homepage" component={Homepage} />
+          <Drawer.Screen name="HomeNavbar" component={HomeNavbar} />
+          <Drawer.Screen name="MyAccount" component={MyAccount} />
         </Drawer.Navigator>
       </NavigationContainer>
+    </Provider>  
   );
 }
---
+++ client/screens/MyAccount.jsx
@@ -0,0 +1,153 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, ScrollView } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { Feather, MaterialIcons, Ionicons, AntDesign, MaterialCommunityIcons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+
+const MyAccount = () => {
+    const navigation = useNavigation();
+
+    return (
+        <LinearGradient
+          style={styles.container}
+          colors={['rgba(140, 165, 253, 1)', 'rgba(243, 129, 245, 0.77)']}
+          start={{ x: 0, y: 0 }}
+          end={{ x: 1, y: 1 }}
+        >
+          <ScrollView>
+            <View style={styles.header}>
+              <TouchableOpacity style={{position: 'absolute'}} onPress={() => navigation.goBack()}>
+                <Feather name="arrow-left" size={24} color="white"/>
+              </TouchableOpacity>
+              <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+                <Text style={styles.greeting}>Hello Juan!</Text>
+              </View>
+            </View>
+            <View style={styles.whiteContainer}>
+              <Text style={styles.accountTag}>Account</Text>
+              <View style={styles.options}>
+                <View style={styles.option}>
+                  <MaterialIcons name="credit-card" size={18} color="black" />
+                    <TouchableOpacity>
+                      <Text style={styles.optionName}>Credit Cards</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.option}>
+                  <Ionicons name="add-circle-outline" size={18} color="black" />
+                  <TouchableOpacity>
+                    <Text style={styles.optionName}>Add Money</Text>
+                  </TouchableOpacity>
+                </View>
+                <View style={styles.option}>
+                  <MaterialIcons name="compare-arrows" size={18} color="black" />
+                  <TouchableOpacity>
+                    <Text style={styles.optionName}>Transfers</Text>
+                  </TouchableOpacity>
+                </View>
+                <View style={styles.option}>
+                  <Feather name="activity" size={18} color="black" />
+                  <TouchableOpacity>
+                    <Text style={styles.optionName}>Activity</Text>
+                  </TouchableOpacity>
+                </View>
+                <View style={styles.option}>
+                  <AntDesign name="contacts" size={18} color="black" />
+                  <TouchableOpacity>
+                    <Text style={styles.optionName}>Contacts</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+
+              <Text style={styles.settingsTag}>Account Settings</Text>
+                <View style={styles.options}>
+                  <View style={styles.option}>
+                    <MaterialCommunityIcons name="account-settings-outline" size={18} color="black" />
+                    <TouchableOpacity>
+                    <Text style={styles.optionName}>Profile</Text>
+                  </TouchableOpacity>
+                  </View>
+                  <View style={styles.option}>
+                    <Feather name="help-circle" size={18} color="black" />
+                    <TouchableOpacity>
+                      <Text style={styles.optionName}>Help</Text>
+                    </TouchableOpacity>
+                  </View>
+                  <View style={styles.option}>
+                    <MaterialCommunityIcons name="logout-variant" size={18} color="black" />
+                    <TouchableOpacity onPress={() => navigation.navigate('LandingPage')}>
+                      <Text style={styles.optionName}>Log Out</Text>
+                    </TouchableOpacity>
+                  </View>
+                </View>
+            </View>
+          </ScrollView>
+        </LinearGradient>
+    )
+}
+
+export default MyAccount;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    display: 'flex',
+    flexDirection: 'row',
+    marginLeft: 18,
+    marginRight: 18,
+    top: 24
+  },
+  greeting: {
+    color: 'white',
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  whiteContainer: {
+    top:50,
+    backgroundColor: 'white',
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    height: '100%'
+  },
+  accountTag: {
+    fontSize: 18,
+    fontWeight: 'normal',
+    color: '#A7A7A7',
+    marginTop: 40,
+    marginBottom: 25,
+    paddingBottom: 15,
+    paddingLeft: 18,
+    paddingRight: 18,
+    borderBottomWidth: 1,
+    borderBottomColor: 'rgba(167, 167, 167, 0.83)',
+    borderStyle: 'solid',
+  },
+  options: {
+    paddingLeft: 18,
+    paddingRight: 18,
+  },
+  option: {
+    display: 'flex',
+    flexDirection: 'row',
+    marginVertical: 20
+  },
+  optionName: {
+    color: 'black',
+    fontSize: 18,
+    fontWeight: 'normal',
+    marginLeft: 30
+  },
+  settingsTag: {
+    fontSize: 18,
+    fontWeight: 'normal',
+    color: '#A7A7A7',
+    marginTop: 15,
+    paddingBottom: 15,
+    paddingLeft: 18,
+    paddingRight: 18,
+    borderBottomWidth: 1,
+    borderBottomColor: 'rgba(167, 167, 167, 0.83)',
+    borderStyle: 'solid',
+  },
+})
