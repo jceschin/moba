994425ff05f39f009069ce5109447691a30176ef
Jose Luis Ramirez <ramirezjoseluis676@gmail.com>
2021-01-26 19:25:43 -0500

    a

--- api/server.js
+++ api/server.js
@@ -1,18 +1,18 @@
-const path = require('path');
-const gateway = require('express-gateway');
-const morgan = require('morgan')
+const path = require("path");
+const gateway = require("express-gateway");
+const morgan = require("morgan");
 
-const {conn} = require('./db.js')
-require('./microservices/users.js')  //Uncomment requires for run all the microservices
-require('./microservices/auth.js')
+const { conn } = require("./db.js");
+require("./microservices/users.js"); //Uncomment requires for run all the microservices
+require("./microservices/auth.js");
 
-require('./microservices/accounts.js')
-require('./microservices/admin.js')
+require("./microservices/accounts.js");
+require("./microservices/admin.js");
+require("./microservices/email.js");
 
-conn.sync({ force: true })
-.then(() => {
-  gateway()
-  .load(path.join(__dirname, 'config'))
-  .run();
-  console.log(`Connected to Database ${conn.config.database}, with user '${conn.config.username}' on port ${conn.config.port}`)
-})
+conn.sync({ force: true }).then(() => {
+  gateway().load(path.join(__dirname, "config")).run();
+  console.log(
+    `Connected to Database ${conn.config.database}, with user '${conn.config.username}' on port ${conn.config.port}`
+  );
+});
