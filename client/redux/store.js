d163ce4b5359b7c0c29448b239950db94e8d5670
jairoPonti <jairmponti@gmail.com>
2021-01-27 22:36:10 -0300

    Todo funcionando

--- client/redux/store.js
+++ client/redux/store.js
@@ -1,6 +1,7 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './rootReducer';
 import thunk from 'redux-thunk';
+
 
 const composeEnhancers = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
 
