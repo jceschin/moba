fef88507e3b568a68c301a177b56263f4d793af0
Ivanovicc <ivanacosta456@gmail.com>
2021-02-08 19:32:55 -0300

    addContactScreens

--- client/screens/AuthUserScreen/AuthUserScreen.js
+++ client/screens/AuthUserScreen/AuthUserScreen.js
@@ -9,13 +9,14 @@ import MyContact from "./MyContact";
 import SendMoney from "./SendMoney";
 import Card from "./Card";
 import AddMoney from "./AddMoney";
+import AddContact from "./AddContact";
 const Stack = createStackNavigator();
 
 export const Context = React.createContext({
   eye: null,
-  setEye: () => {},
+  setEye: () => { },
   toggle: null,
-  setToggle: () => {},
+  setToggle: () => { },
 });
 
 const RootStackScreen = ({ navigation }) => {
@@ -30,6 +31,7 @@ const RootStackScreen = ({ navigation }) => {
           <Stack.Screen name="MyAccount" component={MyAccount} />
           <Stack.Screen name="AccountNumber" component={AccountNumber} />
           <Stack.Screen name="MyContacts" component={MyContacts} />
+          <Stack.Screen name="AddContact" component={AddContact} />
           <Stack.Screen name="MyContact" component={MyContact} />
           <Stack.Screen name="SendMoney" component={SendMoney} />
           <Stack.Screen name="AddMoney" component={AddMoney} />
