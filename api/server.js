34f1e4f950f94b0dcc9a8ac5808472f7339a7f55
facu98 <facuortiz.rg@gmail.com>
2021-02-21 23:32:02 -0300

    heroku

--- api/server.js
+++ api/server.js
@@ -3,16 +3,16 @@ const gateway = require("express-gateway");
 const morgan = require("morgan");
 
 const {conn} = require('./db.js');
-require('./microservices/users.js');  //Uncomment requires for run all the microservices
-require('./microservices/email.js');
-require('./microservices/auth.js');
-require('./microservices/transaction.js');
-require("./microservices/accounts.js");
-require("./microservices/admin.js");
-require("./microservices/contacts.js");
-require("./microservices/statistics.js");
-require("./microservices/whatsapp.js");
-require("./microservices/interoperabilities.js");
+// require('./microservices/users.js');  //Uncomment requires for run all the microservices
+// require('./microservices/email.js');
+// require('./microservices/auth.js');
+// require('./microservices/transaction.js');
+// require("./microservices/accounts.js");
+// require("./microservices/admin.js");
+// require("./microservices/contacts.js");
+// require("./microservices/statistics.js");
+// require("./microservices/whatsapp.js");
+// require("./microservices/interoperabilities.js");
 
 conn.sync({ force: false })
 .then(() => {
